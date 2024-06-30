985e8b886e76772bbcce2926353885d1d3a5c1ff
DAVI-ARON <daviaronalfa@gmail.com>
2024-06-30 13:43:27 -0300

    validando os campos na hora da edicao de usuario

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -20,7 +21,7 @@ class UserController extends Controller
     }
 
     public function store(StoreUserRequest $request) {
-        User::create($request->all());
+        User::create($request->validated());
 
         return redirect()->route('users.index')->with('success', 'Usuário inscrito com sucesso!');
     }
@@ -33,15 +34,17 @@ class UserController extends Controller
         return view('admin.users.edit', compact('user'));
     }
 
-    public function update(Request $request, string $id) {
+    public function update(UpdateUserRequest $request, string $id) {
         if(!$user = User::find($id)) {
             return back()->with('Warning', 'Usuário não encontrado!');
         }
 
-        $user->update($request->only([
-            'name',
-            'email'
-        ]));
+        $data = $request->only('name', 'email');
+        if($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
 
         return redirect()->route('users.index')
             ->with('success', 'Usuário editado com sucesso!');
